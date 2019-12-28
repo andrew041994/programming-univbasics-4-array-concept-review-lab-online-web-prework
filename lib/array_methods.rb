d70f10a4150c1276a64ba4b6977e4be34a148745
Alehandro Persaud <andrew041994@gmail.com>
2019-12-28 17:09:46 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def find_element_index(scale,value_to_find)
+  if scale.index(value_to_find)
+    scale.index(value_to_find)
+  else
+    nil
+  end
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value(scale_2)
+  scale_2.max
 end
 
-def find_min_value(array)
-  # Add your solution here
+def find_min_value(zig_zag)
+  zig_zag.min
 end
